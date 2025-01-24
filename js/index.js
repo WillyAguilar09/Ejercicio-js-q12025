76d8c4ed89309a27c26972ef004adb291143476f
WillyAguilar09 <jwsellbycar@gmail.com>
2025-01-23 21:00:57 -0600

    Create Index

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let strA=document.getElementById("valorA").value
+    alert(strA)
+}
+function limpiar(){
+    document.getElementById("valorA").value=""
+    document.getElementById("valorB").value=""
+    document.getElementById("valorC").value=""
+    document.getElementById("X1").value=""
+    document.getElementById("X2").value=""
+}
